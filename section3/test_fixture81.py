e2f7c582ce0e98cfb3c4fdfabfb6f31e687e753d
Osteron <dimaslepyan@gmail.com>
2023-01-14 21:11:21 +0300

    added files of lesson3 and pytest.ini

---
+++ section3/test_fixture81.py
@@ -0,0 +1,29 @@
+"""pytest -s -v -m "smoke and win10" test_fixture81.py"""
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+LINK = 'http://selenium1py.pythonanywhere.com/'
+
+
+@pytest.fixture(scope='function')
+def browser():
+    print('\nstart browser for test..')
+    browser = webdriver.Chrome()
+    yield browser
+    print('\nquit browser..')
+    browser.quit()
+
+
+class TestMainPage1:
+
+    @pytest.mark.skip
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(LINK)
+        browser.find_element(By.CSS_SELECTOR, '#login_link')
+
+    @pytest.mark.smoke
+    @pytest.mark.win10
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(LINK)
+        browser.find_element(By.CSS_SELECTOR, '.basket-mini .btn-group > a')
