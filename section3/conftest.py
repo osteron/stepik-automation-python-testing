760ce459cffe0fb26ce6d9f9f58f76aac08d5517
Osteron <dimaslepyan@gmail.com>
2023-01-15 11:40:31 +0300

    added files of lesson 6 step 9 'parser' and 'rerun'; modify conftest.py and requirements.txt

--- section3/conftest.py
+++ section3/conftest.py
@@ -1,22 +1,32 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as OptionsFirefox
 
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
+    parser.addoption('--browser_name', action='store', default='Chrome',
                      help='Choose browser: chrome or firefox')
+    parser.addoption('--language', action='store', default=None,
+                     help='Choose language: ru or en')
 
 
 @pytest.fixture(scope='function')
 def browser(request):
     browser_name = request.config.getoption('browser_name')
     browser = None
+    user_language = request.config.getoption('language')
+    options = Options()
+    options.add_experimental_option(
+        'prefs', {'intl.accept_languages': user_language})
+    options_firefox = OptionsFirefox()
+    options_firefox.set_preference("intl.accept_languages", user_language)
     if browser_name.lower() == 'chrome':
         print('\nstart chrome browser fot test..')
-        browser = webdriver.Chrome()
+        browser = webdriver.Chrome(options=options)
     elif browser_name.lower() == 'firefox':
         print('\nstart firefox browser for test..')
-        browser = webdriver.Firefox()
+        browser = webdriver.Firefox(firefox_profile=options_firefox)
     else:
         raise pytest.UsageError('--browser_name should be chrome or firefox')
     yield browser
